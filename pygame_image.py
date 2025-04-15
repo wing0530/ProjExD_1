3ea96e77a94396879ba20d942a7783f060ca21ba
tsubasa <c0a2417498@edu.teu.ac.jp>
2025-04-15 15:39:25 +0900

    8

--- pygame_image.py
+++ pygame_image.py
@@ -9,7 +9,9 @@ def main():
     pg.display.set_caption("はばたけ！こうかとん")
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
-    bg_img = pg.image.load("fig/pg_bg.jpg")
+    original1 = pg.image.load("fig/pg_bg.jpg")
+    flipped1 = pg.transform.flip(original1,True,False)
+    original2 = pg.image.load("fig/pg_bg.jpg")
     img = pg.image.load("fig/3.png")
     img = pg.transform.flip(img,True,False)
     tmr = 0
@@ -17,8 +19,9 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        screen.blit(bg_img, [-tmr, 0])
-        screen.blit(bg_img, [1600-tmr, 0])
+        screen.blit(original1, [-tmr, 0])
+        screen.blit(flipped1, [1600-tmr, 0])
+        screen.blit(original2, [3200-tmr, 0])
         screen.blit(img,[300,200])
         pg.display.update()
         tmr += 1        

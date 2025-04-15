2488b495e1545ddc5f951291186929f2dea93fea
tsubasa <c0a2417498@edu.teu.ac.jp>
2025-04-15 15:29:05 +0900

    背景修正

--- pygame_image.py
+++ pygame_image.py
@@ -17,8 +17,8 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        screen.blit(bg_img, [0, 0])
-        screen.blit(img,[tmr,200])
+        screen.blit(bg_img, [-tmr, 0])
+        screen.blit(img,[300,200])
         pg.display.update()
         tmr += 1        
         clock.tick(200)

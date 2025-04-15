1dd58b23a274a6484291363d9bb7ca2817f20558
tsubasa <c0a2417498@edu.teu.ac.jp>
2025-04-15 15:31:39 +0900

    7

--- pygame_image.py
+++ pygame_image.py
@@ -18,6 +18,7 @@ def main():
             if event.type == pg.QUIT: return
 
         screen.blit(bg_img, [-tmr, 0])
+        screen.blit(bg_img, [1600-tmr, 0])
         screen.blit(img,[300,200])
         pg.display.update()
         tmr += 1        
